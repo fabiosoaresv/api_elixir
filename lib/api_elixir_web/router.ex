3ee535580695ce04388a30c4415335e815fe6270
Fabio Soares Venturelli <fabio.venturelli@meta.com.br>
2021-03-18 14:14:59 -0300

    Update routes API

--- lib/api_elixir_web/router.ex
+++ lib/api_elixir_web/router.ex
@@ -9,19 +9,17 @@ defmodule ApiElixirWeb.Router do
     pipe_through :api
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  pipeline :browser do
+    plug(:accepts, ["html"])
+  end
 
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: ApiElixirWeb.Telemetry
-    end
+  scope "/", ApiElixirWeb do
+    pipe_through :browser
+    get "/", DefaultController, :index
+  end
+
+  scope "/api", ApiElixirWeb do
+    pipe_through :api
+    resources "/businesses", BusinessController, except: [:new, :edit]
   end
 end
